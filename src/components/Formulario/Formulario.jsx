d7004fc91292bc6f813fb09ceca97e9c50b8e82b
cristiangaby112 <cristiangabrielbarriento@outlook.com>
2022-11-23 22:13:54 -0300

    detalles en el formulario

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import s from "./Formulario.module.css";
-import { toast, ToastContainer } from "react-toastify";
+// import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Formulario = () => {
   const [state, handleSubmit] = useForm("xeqnrzqw");
   const id = "message";
-  const mensaje = () => {
-    toast.success("Gracias por comunicar con cristian", {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
+  // const mensaje = () => {
+  //   toast.success("Gracias por comunicar con cristian", {
+  //     position: "top-right",
+  //     autoClose: 5000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //   });
+  // };
   // useEffect(() =>{
   //   if(state.succeeded){
   //     mensaje();
