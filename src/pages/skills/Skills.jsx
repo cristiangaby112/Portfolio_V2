236f1c18cbd05585d0728228f72a36d70045cb99
cristiangaby112 <cristiangabrielbarriento@outlook.com>
2022-11-25 20:10:34 -0300

    skill agregado

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -3,11 +3,15 @@ import s from './Skills.module.css';
 import SkillCard from '../../components/skillCard/SkillCard';
 import dataSkills from './dataSkills';
 import Title from '../../components/title/Title';
+import { useTranslation } from "react-i18next";
 
 const Skills = () => {
+  const { t } = useTranslation("translation");
+
+
   return (
     <div id="skills" className= {s.container_skills_title}>
-      <Title title={"Habilidades"}/>
+      <Title title={t('Container_Skills.Habilidades')}/>
       <div className={s.container_skills}>
         {
           dataSkills?.map(skill => {
